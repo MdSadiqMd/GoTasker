05b6a508047abcfd89842a2ef22461a90d48057f
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-11-16 11:58:39 +0530

    🐢 terminal flags setup

--- package/command/command.go
+++ package/command/command.go
@@ -1,1 +1,20 @@
 package command
+
+import (
+	"flag"
+
+	"github.com/MdSadiqMd/GoTasker/package/types"
+)
+
+func NewCmdFlags() *types.CmdFlags {
+	cf := types.CmdFlags{}
+
+	flag.StringVar(&cf.Add, "add", "", "Add a new todo specify title")
+	flag.StringVar(&cf.Edit, "edit", "", "Edit a todo by index & specify a new title. id:new_title")
+	flag.IntVar(&cf.Del, "del", -1, "Specify todo by index to delete")
+	flag.IntVar(&cf.Toggle, "toggle", -1, "Specify todo by index to toggle complete true/false")
+	flag.BoolVar(&cf.List, "list", false, "List all todos")
+	flag.Parse()
+
+	return &cf
+}
