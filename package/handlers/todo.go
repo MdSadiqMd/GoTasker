d7b3f1173f6de6df62d8f90b4991a7deabb05d0b
Md.Sadiq <mohammadsadiq4950@gmail.com>
2024-11-16 10:54:42 +0530

    ➕ ➖ 🗑️ functionalities added

--- package/handlers/todo.go
+++ package/handlers/todo.go
@@ -1,5 +1,38 @@
 package handlers
 
-import "github.com/MdSadiqMd/GoTasker/package/types"
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/MdSadiqMd/GoTasker/package/types"
+)
 
 type Todos []types.Todo
+
+func (todos *Todos) add(title string) {
+	todo := types.Todo{
+		Title:       title,
+		Completed:   false,
+		CompletedAt: nil,
+		CreatedAt:   time.Now(),
+	}
+	*todos = append(*todos, todo)
+}
+
+func (todos *Todos) validateIndex(index int) error {
+	if index < 0 || index >= len(*todos) {
+		err := errors.New("invalid index")
+		fmt.Println(err)
+		return err
+	}
+	return nil
+}
+
+func (todos *Todos) delete(index int) error {
+	if err := todos.validateIndex(index); err != nil {
+		return err
+	}
+	*todos = append((*todos)[:index], (*todos)[index+1:]...)
+	return nil
+}
